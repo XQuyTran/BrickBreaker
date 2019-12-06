2a4f325ff5b35e311d4ab0a28a786a89238fe326
XQuyTran <55830692+XQuyTran@users.noreply.github.com>
2019-12-06 20:27:07 +0700

    update load level function

--- BrickBreaker/Brick_Breaker.cpp
+++ BrickBreaker/Brick_Breaker.cpp
@@ -657,9 +657,9 @@ void BrickBreaker::startMenu()
 			if (Keyboard::isKeyPressed(Keyboard::Enter))
 			{
 				int level;
-				for (int i = 0; i < rand() % 4 + 1; i++)
+				for (int i = 0; i < rand() % 6 + 1; i++)
 				{
-					level = rand() % 5 + 1;
+					level = rand() % 6 + 1;
 				}
 
 				loadLevel(level);
@@ -802,36 +802,4 @@ int BrickBreaker::getRemainLives()
 int BrickBreaker::getRemainBricks()
 {
 	return brick_list.size();
-}
-
-void Brick_Breaker()
-{
-	BrickBreaker brick_game;
-	FloatRect topEdge(0, 68, wWidth, 1), leftEdge(0, 68, 1, wHeight - 68), rightEdge(wWidth - 1, 68, 1, wHeight - 68), lowerEdge(0, wHeight - 1, wWidth, 1);
-	Texture	texture;
-	Sprite sprite;
-	
-	//brick_game.loadLevel(0);
-	if (!texture.loadFromFile("BrickBreakerData/image/3.jpg"))
-	{
-		std::cout << "Load Textured failed";
-	}
-	sprite.setTexture(texture);
-	sprite.setTextureRect(IntRect(0, 0, wWidth, wHeight));
-	
-	
-	while (brick_game.isOpen())
-	{
-		brick_game.startMenu();
-		brick_game.handleKeyPress();
-		brick_game.handleEdgeCollision(topEdge, leftEdge, rightEdge, lowerEdge);
-		brick_game.handleBrickCollision();
-		brick_game.Countdown_timer();
-		brick_game.draw_game(sprite);
-		if (brick_game.getRemainLives() == 0 || brick_game.getRemainBricks() == 0)
-		{
-			brick_game.gameOverHandle(sprite);
-		}
-		
-	}
 }
--- BrickBreaker/main.cpp
+++ BrickBreaker/main.cpp
@@ -3,5 +3,31 @@
 
 int main()
 {
-	Brick_Breaker();
+	BrickBreaker brick_game;
+	FloatRect topEdge(0, 68, wWidth, 1), leftEdge(0, 68, 1, wHeight - 68), rightEdge(wWidth - 1, 68, 1, wHeight - 68), lowerEdge(0, wHeight - 1, wWidth, 1);
+	Texture	texture;
+	Sprite sprite;
+
+	//brick_game.loadLevel(0);
+	if (!texture.loadFromFile("BrickBreakerData/image/3.jpg"))
+	{
+		std::cout << "Load Textured failed";
+	}
+	sprite.setTexture(texture);
+	sprite.setTextureRect(IntRect(0, 0, wWidth, wHeight));
+
+
+	while (brick_game.isOpen())
+	{
+		brick_game.startMenu();
+		brick_game.handleKeyPress();
+		brick_game.handleEdgeCollision(topEdge, leftEdge, rightEdge, lowerEdge);
+		brick_game.handleBrickCollision();
+		brick_game.Countdown_timer();
+		if (brick_game.getRemainLives() == 0 || brick_game.getRemainBricks() == 0)
+		{
+			brick_game.gameOverHandle(sprite);
+		}
+		brick_game.draw_game(sprite);
+	}
 }
